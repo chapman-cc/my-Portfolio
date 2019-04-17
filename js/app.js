92265afb3587b3b3fb8b4048cfda3a6acc5b3c19
chapmanCC <chapboy1989@gmail.com>
2019-04-17 17:11:29 +0800

    sixth complete

--- js/app.js
+++ js/app.js
@@ -1,42 +1,54 @@
-const projectList = document.querySelector(".project__list");
+const projectSection = document.querySelector(".project");
 
-const projectDetails = [
+const projects = [
     {
         githubLink: "https://github.com/chapweb/employees-directory-mockup",
+        githubDemo: "https://chapweb.github.io/employees-directory-mockup/",
         imgSrc: "./img/employee-directory.jpg",
         title: "Employee Directory",
-        descrip: "Using JS Fetch API to draw employee details and display in a modal viewer" 
+        desc: "Using JS Fetch API to draw employee details and display in a modal viewer" 
     },
     {
         githubLink: "https://github.com/chapweb/dashboard_mockup",
+        githubDemo: "https://chapweb.github.io/dashboard_mockup/",
         imgSrc: "./img/dashboard.jpg",
         title: "Web dashboard",
-        descrip: "Create a dashboard mockup from scratch, with CSS grid and Chart.js plugin" 
+        desc: "Create a dashboard mockup from scratch, with CSS grid and Chart.js plugin" 
     },
     {
         githubLink: "https://github.com/chapweb/thtechdegree_unit03",
+        githubDemo: "https://chapweb.github.io/thtechdegree_unit03/",
         imgSrc: "./img/form-styling.jpg",
         title: "HTML CSS Form",
-        descrip: "Create a form mockup from with HTML form tags and CSS" 
+        desc: "Create a form mockup from with HTML form tags and CSS" 
     },
     {
         githubLink: "https://github.com/chapweb/js_lightbox",
+        githubDemo: "https://chapweb.github.io/js_lightbox/",
         imgSrc: "./img/photo-lightbox.jpg",
         title: "JS Lightbox",
-        descrip: "Create lightbox with JS plugin" 
+        desc: "Create lightbox with JS plugin" 
     },
     {
         githubLink: "https://github.com/chapweb/js_hangman",
+        githubDemo: "https://chapweb.github.io/js_hangman/",
         imgSrc: "./img/hangman.jpg",
         title: "jQuery Hangman Game",
-        descrip: "An excercise of Hangman game made with jQuery" 
+        desc: "An excercise of Hangman game made with jQuery" 
     }
 ]
 
-`<li class="project__item">
-<a href="" target="_blank">
-    <img src="" alt="" class="project__preview">
-    <h3 class="project__title"></h3>
-    <p class="project__desc"></p>
-</a>
-</li>`
+for (const project of projects) {
+    let html = `
+        <li class="project__item">
+            <img src="${project.imgSrc}" alt="${project.title}" class="project__preview">
+            <h3 class="project__title">${project.title}</h3>
+            <p class="project__desc">${project.desc}</p>
+            <div class="project__help">
+                <a href="${project.githubLink}" target="_blank">GitHub</a> <a href="${project.githubDemo}" target="_blank">Demo</a>
+            </div>
+        </li>
+    `;
+    projectSection.innerHTML += html;
+    
+}
